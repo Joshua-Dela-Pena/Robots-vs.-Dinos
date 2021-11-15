cbb3b8899511eb306ce9b44cec30ec8da429b17e
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2021-11-15 09:47:28 -0600

    I created my dinosaur class and created a function that appends dinosaurs 1 to 3 to append into my dinosaur herd

--- herd.py
+++ herd.py
@@ -1,20 +1,15 @@
 from dinosaur import Dinosaurs
 
-
-
-
-class Herd: 
+class Herd:
     def __init__(self):
-        self.dinosaur_herd = [] 
+        self.dinosaur_herd = []
         self.create_herd()
+        
     def create_herd(self):
-        dinosaur_one = Dinosaurs("oll", 50, 500)
-        dinosaur_two = Dinosaurs("llo", 50, 500)
-        dinosaur_three = Dinosaurs("lol", 50, 500)
-
-        self.dinosaur_herd.append(dinosaur_one)
-        self.dinosaur_herd.append(dinosaur_two)
-        self.dinosaur_herd.append(dinosaur_three)
-
-    
-   
+            dino_one = Dinosaurs("oll", 50, 100)
+            dino_two = Dinosaurs("llo", 50, 100)
+            dino_three = Dinosaurs("lol", 50, 100)
+            
+            self.dinosaur_herd(dino_one)
+            self.dinosaur_herd(dino_two)
+            self.dinosaur_herd(dino_three)
